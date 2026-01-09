531f59ab5e2c544f48c370833eb72456d915ff8a
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-09 19:44:04 +0545

    Customer page creation

---
+++ src/pages/auth/CustomerPage.tsx
@@ -0,0 +1,10 @@
+export default function CustomerPage() {
+  return (
+    <>
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold">This is Customer Page</h1>
+        <p>Caution⚠️: Customer Page is under construction. Soon, It will be available to you...</p>
+      </div>
+    </>
+  )
+};
