f69029e261890aecc90120d1db5508da4cb25539
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-09 19:44:32 +0545

    forget password page created

---
+++ src/pages/auth/ForgetPasswordPage.tsx
@@ -0,0 +1,29 @@
+import { PageHeadingWithSubtitle } from "../../components/page-heading/PageHeading";
+export default function ForgetPasswordPage() {
+
+  // interpolation {}
+  // <></> -> fragmentation
+  
+  return (
+  <section className="h-screen flex flex-col items-center justify-center p-2">
+    <div className="w-full md:w-2xl lg:w-3xl xl:2-4xl shadow-2xl bg-stone-200/75 rounded-tl-[10px] rounded-br-[10px] p-5 py-10">
+
+      <PageHeadingWithSubtitle title="Reset Your Password">
+          You can reset your password from here...
+      </PageHeadingWithSubtitle>
+
+      <div className="w-full flex flex-col gap-5">
+        <span className="flex items-center">
+          <span className="h-px flex-1 bg-gray-300"></span>
+          <span className="shrink-0 px-4 text-gray-900">Or</span>
+          <span className="h-px flex-1 bg-gray-300"></span>
+        </span>
+
+        <div className="flex w-full justify-center">
+          <a href="/login" className="cursor-pointer hover:underline w-full p-2 bg-[#ffffff] text-green-950 rounded-full border border-[#002700] text-center">Login Here</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
