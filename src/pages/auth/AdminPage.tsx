0a298f16ecb757c39e2c9889158e55de9e12dae6
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-09 19:43:43 +0545

    Admin page creation

---
+++ src/pages/auth/AdminPage.tsx
@@ -0,0 +1,10 @@
+export default function AdminPage() {
+  return (
+    <>
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold">This is Admin Page</h1>
+        <p>Caution⚠️: Admin Page is under construction. Soon, It will be available to you...</p>
+      </div>
+    </>
+  )
+};
