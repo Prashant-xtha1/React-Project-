325fcf0914e9f0913e919b0d10b23cff62aa0072
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-09 19:42:43 +0545

    Divider Added

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -12,7 +12,7 @@ export default function LoginPage() {
   
   return (
   <section className="h-screen flex flex-col items-center justify-center p-2">
-    <div className="w-full md:w-2xl lg:w-3xl xl:2-4xl border border-[#a1a1a1] bg-[#f1f1f1] rounded-tl-[10px] rounded-br-[10px] p-5 py-10">
+    <div className="w-full md:w-2xl lg:w-3xl xl:2-4xl shadow-2xl bg-stone-200/75 rounded-tl-[10px] rounded-br-[10px] p-5 py-10">
 
       {/* <PageHeadingTitle  />
       <PageHeadingSubtitle>
@@ -22,7 +22,20 @@ export default function LoginPage() {
       <PageHeadingWithSubtitle title="Login From Here">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quam ut eaque atque beatae? Minima
       </PageHeadingWithSubtitle>
+
       <LoginForm />
+
+      <div className="w-full flex flex-col gap-5">
+        <span className="flex items-center">
+          <span className="h-px flex-1 bg-gray-300"></span>
+          <span className="shrink-0 px-4 text-gray-900">Or</span>
+          <span className="h-px flex-1 bg-gray-300"></span>
+        </span>
+
+        <div className="flex w-full justify-center">
+          <a href="/register" className="cursor-pointer hover:underline w-full p-2 bg-[#ffffff] text-green-950 rounded-full border border-[#002700] text-center">Register Here</a>
+        </div>
+      </div>
     </div>
   </section>
   )
