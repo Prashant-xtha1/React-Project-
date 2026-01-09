85fc12dc304e9eff281d52cf9c6ccb142c37a8bc
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-09 19:41:20 +0545

    RouterConfig Added

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client"
-import LoginPage from "./pages/auth/LoginPage";
 import "./assets/globals.css"
+import RouterConfig from "./router/RouterConfig";
 // <div id="root"></div>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LoginPage />
+    <RouterConfig />
   </StrictMode>
 );
 
