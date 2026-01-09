7768b017734fdddc8461f29d4557d8ecd6aaba77
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-09 19:45:53 +0545

    seller page created

---
+++ src/pages/auth/SellerPage.tsx
@@ -0,0 +1,10 @@
+export default function SellerPage() {
+  return (
+    <>
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold">This is Seller Page</h1>
+        <p>Caution⚠️: Seller Page is under construction. Soon, It will be available to you...</p>
+      </div>
+    </>
+  )
+};
