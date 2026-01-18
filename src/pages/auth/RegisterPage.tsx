4f00c1843020aa4b5f384675fb6f49d74a23fd2f
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-18 18:48:38 +0545

    removed unused code

--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,8 +1,20 @@
-export default function RegisterPage() {
+// interpolation {}
+// <></> -> fragmentation
+import { useEffect } from "react";
+import { useOutletContext } from "react-router";
+import type { IOutletContext } from "../layout/AuthLayout";
 
-  // interpolation {}
-  // <></> -> fragmentation
-  
+export default function RegisterPage() {
+  const outletContext = useOutletContext<IOutletContext>();
+  useEffect(() => {
+    outletContext.setLayoutData({
+      pageTitle: "Register From Here",
+      subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quam ut eaque atque beatae?",
+      btnTxt: "Login Here",
+      btnLink: "/"
+    })
+  }, []);
+
   return (
   <>
 
