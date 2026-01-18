c8689c5a63f457a325735255a922a2addda22868
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-18 18:47:29 +0545

    react hook handler implement

--- src/components/form/FormInput.tsx
+++ src/components/form/FormInput.tsx
@@ -1,25 +1,36 @@
 // Generic FormInput
-export const FormInput = ({type, name, placeholder}: Readonly<{type: string, name: string, placeholder: string}>) => {
+export const FormInput = ({ type, name, placeholder, handler }: Readonly<{
+  type?: string,
+  name: string,
+  placeholder?: string,
+  // eslint-disable-next-line
+  handler: any
+}>) => {
   return (
     <>
-      <input className="rounded-[10px] border border-[#5d5d5d] w-full p-2" 
-      type={type} 
-      name={name} 
-      id={name} 
-      placeholder={placeholder} />
+      <input className="rounded-[10px] border border-[#5d5d5d] w-full p-2"
+        type={type}
+        {...handler(name)}
+        id={name}
+        placeholder={placeholder} />
     </>
   );
 };
 
 // Specific FormInput
-export const EmailInput = ({name, placeholder}: Readonly<{name: string, placeholder: string}>) => {
+export const EmailInput = ({ name, placeholder, handler }: Readonly<{
+  name: string,
+  placeholder?: string,
+  // eslint-disable-next-line
+  handler: any
+}>) => {
   return (
     <>
-      <input className="rounded-[10px] border border-[#5d5d5d] w-full p-2" 
-      type="email" 
-      name={name} 
-      id={name} 
-      placeholder={placeholder} />
+      <input className="rounded-[10px] border border-[#5d5d5d] w-full p-2"
+        type="email"
+        {...handler(name)}
+        id={name}
+        placeholder={placeholder} />
     </>
   );
 };
