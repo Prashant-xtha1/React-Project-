815c44b02fdef29e45f5a58620347ef64a1ed8fe
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-18 18:48:04 +0545

    Minor update

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,10 +1,22 @@
 // import done like this if the function is exported in default
 import LoginForm from "../../components/auth/LoginForm";
+import { useOutletContext } from "react-router";
+import { useEffect } from "react";
+import type { IOutletContext } from "../layout/AuthLayout";
 
 export default function LoginPage() {
-
   // interpolation {}
   // <></> -> fragmentation
+
+  const outletContext = useOutletContext<IOutletContext>();
+  useEffect(() => {
+    outletContext.setLayoutData({
+      pageTitle: "Login From Here",
+      subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quam ut eaque atque beatae?",
+      btnTxt: "Register Here",
+      btnLink: "/register"
+    })
+  }, []);
   
   return (
     <>
