83ebead3dc156968f92577292446579c028c61da
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-20 18:57:34 +0545

    sonner toaster used

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client"
 import "./assets/globals.css"
 import RouterConfig from "./router/RouterConfig";
+import { Toaster } from "sonner";
 // <div id="root"></div>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster richColors closeButton/>
     <RouterConfig />
   </StrictMode>
 );
