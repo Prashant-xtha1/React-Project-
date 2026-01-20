3a85d2a1dea3e5065f8efc261f937b06e9192077
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-20 18:56:22 +0545

    Navigation Implemented

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,32 +1,48 @@
 import { FormCancelButton, FormSubmitButton } from "../form/FormAction";
 import { FormInputControl } from "../form/FormInput";
 import { FormLabel } from "../form/FormLabel"
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import { useForm } from "react-hook-form";
+
 import { LoginDTO, type ICredentials } from "./auth.contract";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Cookies from "js-cookie"
+import axiosInstance from "../../config/axios.config";
+import { toast } from "sonner";
 
 export default function LoginForm() {
   // using hook useForm
-  const {control, handleSubmit, formState: {errors} } = useForm({
+  const { control, handleSubmit, formState: { errors, isSubmitting } } = useForm({
     defaultValues: { email: "", password: "", },
     resolver: zodResolver(LoginDTO),
   });
 
+  const navigate = useNavigate();
 
-  const submitForm = (credentials: ICredentials) => {
-    console.log("Submitted Successfully", credentials);
+  const submitForm = async (credentials: ICredentials) => {
+    try {
+      const response = await axiosInstance.post("auth/login", credentials);
+      Cookies.set("token", response.data, {
+        expires: 1,
+        secure: true,
+        sameSite: "Lax"
+      })
+      const loggedInUser = await axiosInstance.get("auth/me");
 
-    Cookies.set("token", "", {
-      expires: 1,
-      secure: true,
-      sameSite: "Lax"
-    })
+      toast.success("Welcome to User Panel, " + loggedInUser.data.name);
+      navigate("/" + loggedInUser.data.role)
+      console.log("Submitted Successfully", credentials);
+      // console.log(response);
+    } catch (exception) {
+      console.log(exception);
+      toast.error("Sorry! Could not login now!!!", {
+        description: "Some problem occured while logging you at this moment, try again later."
+      })
+    };
   }
 
-  console.log(errors)
-  return(
+  // console.log(errors)
+  return (
     <>
       <form onSubmit={handleSubmit(submitForm)} id="loginForm" className="flex flex-col gap-5 p-5">
 
@@ -37,7 +53,7 @@ export default function LoginForm() {
             {/* <FormInput type="email" name="email" placeholder="Enter your email as username" /> */}
             {/* <FormInputControl name="email" type="email" placeholder="Enter your email as username" handler={control} /> */}
 
-            <FormInputControl <ICredentials> name="email" type="email" control={control} placeholder="Enter your email as username" errMsg= {errors?.email?.message} />
+            <FormInputControl <ICredentials> name="email" type="email" control={control} placeholder="Enter your email as username" errMsg={errors?.email?.message} />
           </div>
         </div>
 
@@ -45,7 +61,7 @@ export default function LoginForm() {
           <FormLabel htmlFor="password">Password: </FormLabel>
           <div className="w-full md:w-3/4">
             {/* <FormInput type="password" name="password" placeholder="Enter your Password" handler={register} /> */}
-            <FormInputControl  name="password" type="password" placeholder="Enter your Password" control={control} errMsg= {errors?.password?.message} />
+            <FormInputControl name="password" type="password" placeholder="Enter your Password" control={control} errMsg={errors?.password?.message} />
           </div>
         </div>
 
@@ -54,8 +70,8 @@ export default function LoginForm() {
         </div>
 
         <div className="flex flex-col md:flex-row w-full items-center gap-5">
-          <FormCancelButton label="Cancel" />
-          <FormSubmitButton label="Login" />
+          <FormCancelButton disabled={isSubmitting} label="Cancel" />
+          <FormSubmitButton disabled={isSubmitting} label="Login" />
         </div>
 
       </form>
