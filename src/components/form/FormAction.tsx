ea2b14946d8b9505881e77add20734c7b1bd0aa9
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-20 18:57:12 +0545

    update

--- src/components/form/FormAction.tsx
+++ src/components/form/FormAction.tsx
@@ -1,19 +1,24 @@
 import type { ReactNode } from "react"
 
-export const FormCancelButton = ({label="Cancel"}: Readonly<{label: ReactNode}>) => {
+export interface IFormActionButtons {
+  label: ReactNode,
+  disabled?: boolean,
+}
+
+export const FormCancelButton = ({ label="Cancel", disabled=false }: Readonly<IFormActionButtons>) => {
   return (
   <>
-    <button className="cursor-pointer hover:underline hover:scale-96 transition w-full p-2 bg-[#7d0000] text-[#ffffff] rounded-[10px] border border-[#7e0000]">
+    <button disabled={disabled} className="cursor-pointer hover:underline hover:scale-96 transition w-full p-2 bg-[#7d0000] text-[#ffffff] rounded-[10px] border border-[#7e0000]">
       {label}
     </button>
   </>
   )
 }
 
-export const FormSubmitButton = ({label="Submit"}: Readonly<{label: ReactNode}>) => {
+export const FormSubmitButton = ({ label="Submit", disabled=false }: Readonly<IFormActionButtons>) => {
   return (
   <>
-    <button className="cursor-pointer hover:underline hover:scale-96 transition w-full p-2 bg-[#002600] text-[#ffffff] rounded-[10px] border border-[#002700]">
+    <button disabled={disabled} className="cursor-pointer hover:underline hover:scale-96 transition w-full p-2 bg-[#002600] text-[#ffffff] rounded-[10px] border border-[#002700]">
       {label}
     </button>
   </>
