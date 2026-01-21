5ab24c0c26508eda12c6b86c3cbeb020057f6817
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-21 14:36:22 +0545

    user contract

--- src/components/auth/auth.contract.ts
+++ src/components/auth/auth.contract.ts
@@ -9,3 +9,18 @@ export const LoginDTO = z.object({
   email: z.email().nonempty().nonoptional(),
   password: z.string().min(8).nonempty().nonoptional(),
 })
+
+export interface IUser {
+  createdAt: Date | string,
+  email: string,
+  image: {
+    publicId: string,
+    url: string,
+    optimizedUrl: string,
+  },
+  name: string,
+  role: string,
+  status: string, 
+  _id: string
+
+}
