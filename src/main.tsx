6ccadb1b34530aa6fb0a14bf2ef56c1579e15b63
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-21 14:37:01 +0545

    Implementing Auth Provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client"
 import "./assets/globals.css"
 import RouterConfig from "./router/RouterConfig";
 import { Toaster } from "sonner";
+import AuthProvider from "./context/providers/AuthProvider";
 // <div id="root"></div>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Toaster richColors closeButton/>
-    <RouterConfig />
+    <AuthProvider>
+      <Toaster richColors closeButton/>
+      <RouterConfig />
+    </AuthProvider>
   </StrictMode>
 );
 
