2b8a95c2fadd293babb24c19cb8230f8ad41e8d8
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-24 16:15:22 +0545

    layout update

--- src/pages/layout/UserLayout.tsx
+++ src/pages/layout/UserLayout.tsx
@@ -1,12 +1,25 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { UserHeader } from "../../components/user-dashboard/UserHeader";
 import { UserSidebar } from "../../components/user-dashboard/UserSidebar";
 import { UserFooter } from "../../components/user-dashboard/UserFooter";
 import type { IUser } from "../../components/auth/auth.contract";
 import { useAuth } from "../../hook/auth";
+import { useEffect } from "react";
+import { toast } from "sonner";
 
 export default function UserLayout() {
   const {loggedInUser} = useAuth();
+  const navigate = useNavigate();
+
+    useEffect(() => {
+    if(!loggedInUser) {
+      toast.info("You are not logged in...", {
+        description: "Login to access dashboard"
+      });
+      navigate("/");
+    }
+  }, [])
+
   return (
     <>
       <UserHeader loggedInUser={loggedInUser as IUser} />
