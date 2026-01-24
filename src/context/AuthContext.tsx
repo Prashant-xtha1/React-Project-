dc2527e8ab330ec37009105d8834e3457bd4cf56
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-24 15:58:19 +0545

    logout context added

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,13 +4,15 @@ import type { ICredentials, IUser } from "../components/auth/auth.contract";
 export interface IAuthContext {
   login: (data: ICredentials) => Promise<IUser | void>,
   getLoggedInUser: () => Promise<IUser | void>,
-  loggedInUser: undefined | IUser
+  loggedInUser: undefined | IUser,
+  logout: () => void
 }
 
 const AuthContext = createContext<IAuthContext>({
   login: async (): Promise<IUser | void> => {},
   getLoggedInUser: async (): Promise<IUser | void> => {},
-  loggedInUser: undefined
+  loggedInUser: undefined,
+  logout: (): void => {}
 });
 
 export default AuthContext
