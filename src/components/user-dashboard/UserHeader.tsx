673ddb73963e9205724bf93e01ab9f281b0fea10
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-24 15:57:32 +0545

    Logout implementation

--- src/components/user-dashboard/UserHeader.tsx
+++ src/components/user-dashboard/UserHeader.tsx
@@ -1,13 +1,30 @@
+import type { BaseSyntheticEvent } from "react"
+import type { IUser } from "../auth/auth.contract"
+import { NavLink, useNavigate } from "react-router"
+import { useAuth } from "../../hook/auth"
+import { toast } from "sonner"
 
-export const UserHeader = () => {
+export const UserHeader = ({ loggedInUser }: Readonly<{ loggedInUser: IUser }>) => {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleLogout = (e: BaseSyntheticEvent) => {
+    e.preventDefault();
+    logout();
+    toast.success("You have been logout successfully...");
+    navigate("/");
+  }
+
   return(<>
     <header className="h-20 bg-green-600 w-full text-white flex flex-col justify-center text-center">
       <div className="flex items-center justify-between w-full px-6">
         <h1 className="text-2xl font-bold">One-Piece</h1>
         <nav className="flex gap-6">
-          <a href="#" className="hover:text-green-200">Profile</a>
-          <a href="#" className="hover:text-green-200">Settings</a>
-          <a href="#" className="hover:text-green-200">Logout</a>
+          <NavLink to={`/${loggedInUser.role}/profiles`} className="hover:text-green-200">Profile</NavLink>
+          <NavLink to={`/${loggedInUser.role}/settings`} className="hover:text-green-200">Settings</NavLink>
+          <button onClick={handleLogout}
+          className="hover:text-green-200 cursor-pointer transition-colors">Logout
+          </button>
         </nav>
       </div>
     </header>
