406ab8edec649acb1923ef4ffc5dbe3c06e085fe
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-24 15:59:52 +0545

    update user layout

--- src/pages/layout/UserLayout.tsx
+++ src/pages/layout/UserLayout.tsx
@@ -2,13 +2,16 @@ import { Outlet } from "react-router";
 import { UserHeader } from "../../components/user-dashboard/UserHeader";
 import { UserSidebar } from "../../components/user-dashboard/UserSidebar";
 import { UserFooter } from "../../components/user-dashboard/UserFooter";
+import type { IUser } from "../../components/auth/auth.contract";
+import { useAuth } from "../../hook/auth";
 
 export default function UserLayout() {
+  const {loggedInUser} = useAuth();
   return (
     <>
-      <UserHeader />
+      <UserHeader loggedInUser={loggedInUser as IUser} />
       <main className="flex w-full h-screen">
-        <UserSidebar />
+        <UserSidebar loggedInUser={loggedInUser as IUser} />
         <section className="p-5 bg-sky-500 text-white m-3 rounded-lg w-full ">
           <Outlet />
         </section>
