110f3fdc36116d4e03f9a198ccf704f8df283994
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-24 23:09:39 +0545

    banner list page intergrate

--- src/router/adminRouter.tsx
+++ src/router/adminRouter.tsx
@@ -1,3 +1,4 @@
+import BannerListPage from "../pages/banners/BannerListPage";
 import UserDashboard from "../pages/dashboard/UserDashboard";
 import ErrorPage from "../pages/ErrorPage";
 import UserLayout from "../pages/layout/UserLayout";
@@ -5,6 +6,7 @@ import UserListPage from "../pages/user/UserListPage";
 
 export const adminRoutes = [{ path: "/admin", element: <UserLayout />, children: [
     { index: true, Component: UserDashboard },
+    { path: "banners", element: <BannerListPage /> },
     { path: "user", element: <UserListPage /> },
     { path: "user/:userId", element: <UserListPage /> },
     { path: "*", element: <ErrorPage code={404} redirectLink="/admin" redirectTxt="Go back to home" /> },
