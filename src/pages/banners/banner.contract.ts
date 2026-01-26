93900273ee964d99ef5632334e69e898df1f4fa6
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-26 22:54:41 +0545

    Update contract

--- src/pages/banners/banner.contract.ts
+++ src/pages/banners/banner.contract.ts
@@ -1,6 +1,15 @@
+import z from "zod"
+
 export interface IBannerCreateData {
   title: string,
   url: string,
   status: string,
-  image: File | null
+  image: File 
 }
+
+export const BannerDTO = z.object({
+  title: z.string().min(3, "Atleast 3 character is required for title").nonempty("Title is required"),
+  url: z.url().nonempty("Url is required"),
+  status: z.string().regex(/^(active|inactive)$/, "Status can only be either Published or Un-Published").nonempty("Status is required"),
+  image: z.file().nonoptional()
+})
