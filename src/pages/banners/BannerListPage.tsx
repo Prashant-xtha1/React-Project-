774f79a776290a6633d859588572759b090c9090
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-26 14:38:28 +0545

    Update listpage

--- src/pages/banners/BannerListPage.tsx
+++ src/pages/banners/BannerListPage.tsx
@@ -1,44 +1,17 @@
+import { FaPen, FaTrash } from "react-icons/fa";
+import { TableHeader } from "../../components/ui/TableHeader";
+
 export default function BannerListPage() {
   return (
     <>
       {/* Wrapper assumes parent section has bg-teal-500 */}
       <div className="p-6">
 
-        {/* Page Header */}
-        <div className="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-          <h2 className="text-2xl font-bold text-white">
-            Banner Management
-          </h2>
-
-          <button className="mt-3 md:mt-0 bg-white text-teal-600 px-5 py-2 rounded-lg font-medium hover:bg-teal-50 transition">
-            + Add Banner
-          </button>
-        </div>
+        {/* Page Header (Title + Search + CTA) */}
+        <TableHeader title= "Banner Management" showSearch={true} btnUrl="/admin/banner/create" btnTxt= "+ Add Banner" />
 
         {/* Card */}
         <div className="bg-white rounded-xl shadow-lg p-6">
-
-          {/* Search Section */}
-          <div className="mb-6">
-            <div className="flex items-center gap-2 w-full md:w-1/2">
-              <input
-                type="text"
-                placeholder="Search banners..."
-                className="flex-1 px-4 py-2 text-gray-700 bg-white border border-gray-300
-                 rounded-lg placeholder-gray-400
-                 focus:outline-none focus:ring-2 focus:ring-teal-500"
-              />
-
-              <button
-                className="px-5 py-2 bg-teal-500 text-white font-medium rounded-lg
-                 hover:bg-teal-600 cursor-pointer transition shadow-sm">
-                Search
-              </button>
-            </div>
-          </div>
-
-
-
 
           {/* Table */}
           <div className="overflow-x-auto">
@@ -61,9 +34,9 @@ export default function BannerListPage() {
 
                   <td className="p-3">
                     <img
-                      src="https://via.placeholder.com/60"
+                      src="https://placehold.co/400x100"
                       alt="Thumbnail"
-                      className="rounded-md"
+                      className="w-25 rounded-md object-cover"
                     />
                   </td>
 
@@ -79,12 +52,126 @@ export default function BannerListPage() {
 
                   <td className="p-3">
                     <div className="flex justify-center gap-2">
-                      <button className="w-9 h-9 rounded-full bg-teal-500 text-white hover:bg-teal-600 transition">
-                        ✏️
+                      <button className="size-10 flex items-center justify-center rounded-full bg-teal-500 text-white hover:bg-teal-600 transition cursor-pointer">
+                        <FaPen className="size-4" />
                       </button>
 
-                      <button className="w-9 h-9 rounded-full bg-red-500 text-white hover:bg-red-600 transition">
-                        🗑️
+                      <button className="size-10 flex items-center justify-center rounded-full bg-red-500 text-white hover:bg-red-600 transition cursor-pointer">
+                        <FaTrash className="size-4" />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+
+              <tbody className="divide-y">
+                <tr className="hover:bg-gray-50">
+                  <td className="p-3 font-medium text-gray-800">
+                    Summer Sale
+                  </td>
+
+                  <td className="p-3">
+                    <img
+                      src="https://placehold.co/400x100"
+                      alt="Thumbnail"
+                      className="w-25 rounded-md object-cover"
+                    />
+                  </td>
+
+                  <td className="p-3">
+                    <span className="px-3 py-1 text-xs rounded-full bg-green-100 text-green-700">
+                      Active
+                    </span>
+                  </td>
+
+                  <td className="p-3 text-teal-600 underline">
+                    https://example.com
+                  </td>
+
+                  <td className="p-3">
+                    <div className="flex justify-center gap-2">
+                      <button className="size-10 flex items-center justify-center rounded-full bg-teal-500 text-white hover:bg-teal-600 transition cursor-pointer">
+                        <FaPen className="size-4" />
+                      </button>
+
+                      <button className="size-10 flex items-center justify-center rounded-full bg-red-500 text-white hover:bg-red-600 transition cursor-pointer">
+                        <FaTrash className="size-4" />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+
+              <tbody className="divide-y">
+                <tr className="hover:bg-gray-50">
+                  <td className="p-3 font-medium text-gray-800">
+                    Summer Sale
+                  </td>
+
+                  <td className="p-3">
+                    <img
+                      src="https://placehold.co/400x100"
+                      alt="Thumbnail"
+                      className="w-25 rounded-md object-cover"
+                    />
+                  </td>
+
+                  <td className="p-3">
+                    <span className="px-3 py-1 text-xs rounded-full bg-green-100 text-green-700">
+                      Active
+                    </span>
+                  </td>
+
+                  <td className="p-3 text-teal-600 underline">
+                    https://example.com
+                  </td>
+
+                  <td className="p-3">
+                    <div className="flex justify-center gap-2">
+                      <button className="size-10 flex items-center justify-center rounded-full bg-teal-500 text-white hover:bg-teal-600 transition cursor-pointer">
+                        <FaPen className="size-4" />
+                      </button>
+
+                      <button className="size-10 flex items-center justify-center rounded-full bg-red-500 text-white hover:bg-red-600 transition cursor-pointer">
+                        <FaTrash className="size-4" />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+
+              <tbody className="divide-y">
+                <tr className="hover:bg-gray-50">
+                  <td className="p-3 font-medium text-gray-800">
+                    Summer Sale
+                  </td>
+
+                  <td className="p-3">
+                    <img
+                      src="https://placehold.co/400x100"
+                      alt="Thumbnail"
+                      className="w-25 rounded-md object-cover"
+                    />
+                  </td>
+
+                  <td className="p-3">
+                    <span className="px-3 py-1 text-xs rounded-full bg-green-100 text-green-700">
+                      Active
+                    </span>
+                  </td>
+
+                  <td className="p-3 text-teal-600 underline">
+                    https://example.com
+                  </td>
+
+                  <td className="p-3">
+                    <div className="flex justify-center gap-2">
+                      <button className="size-10 flex items-center justify-center rounded-full bg-teal-500 text-white hover:bg-teal-600 transition cursor-pointer">
+                        <FaPen className="size-4" />
+                      </button>
+
+                      <button className="size-10 flex items-center justify-center rounded-full bg-red-500 text-white hover:bg-red-600 transition cursor-pointer">
+                        <FaTrash className="size-4" />
                       </button>
                     </div>
                   </td>
@@ -96,11 +183,11 @@ export default function BannerListPage() {
           {/* Pagination */}
           <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-3 mt-6">
             <p className="text-sm text-gray-600">
-              Showing 1 to 10 of 50 entries
+              Showing 1 to 5 of 50 entries
             </p>
 
             <div className="flex gap-2">
-              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100">
+              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100 cursor-pointer">
                 Prev
               </button>
 
@@ -108,11 +195,11 @@ export default function BannerListPage() {
                 1
               </button>
 
-              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100">
+              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100 cursor-pointer">
                 2
               </button>
 
-              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100">
+              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100 cursor-pointer">
                 Next
               </button>
             </div>
