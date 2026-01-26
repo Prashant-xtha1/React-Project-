b4eb5aeb857001a416e3a24340e4c5e734756c8c
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-01-26 22:16:44 +0545

    Update clean code

--- src/router/adminRouter.tsx
+++ src/router/adminRouter.tsx
@@ -1,13 +1,19 @@
+import BannerCreatePage from "../pages/banners/BannerCreatePage";
 import BannerListPage from "../pages/banners/BannerListPage";
 import UserDashboard from "../pages/dashboard/UserDashboard";
 import ErrorPage from "../pages/ErrorPage";
 import UserLayout from "../pages/layout/UserLayout";
 import UserListPage from "../pages/user/UserListPage";
 
-export const adminRoutes = [{ path: "/admin", element: <UserLayout />, children: [
+export const adminRoutes = [{ 
+  path: "/admin", element: <UserLayout />, children: [
     { index: true, Component: UserDashboard },
+
     { path: "banners", element: <BannerListPage /> },
+    { path: "banner/create", element: <BannerCreatePage /> },
+
     { path: "user", element: <UserListPage /> },
     { path: "user/:userId", element: <UserListPage /> },
     { path: "*", element: <ErrorPage code={404} redirectLink="/admin" redirectTxt="Go back to home" /> },
-  ] },]
+  ] 
+},]
