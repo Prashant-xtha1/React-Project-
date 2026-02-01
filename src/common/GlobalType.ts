c7208f65ffa140ec5abcf68eae66e0da383acbb3
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-02-01 22:36:19 +0545

    Updating global type

--- src/common/GlobalType.ts
+++ src/common/GlobalType.ts
@@ -3,3 +3,19 @@ export interface IImage {
   url: string,
   optimizedUrl: string
 }
+
+export interface IPagination {
+  limit: number,
+  page: number,
+  total: number,
+  totalNoOfPages: number,
+}
+
+export interface IResponse<T> {
+  data: Array<T>, 
+  status: string,
+  message: string,  
+  meta: {
+    pagination: IPagination
+  }
+}
