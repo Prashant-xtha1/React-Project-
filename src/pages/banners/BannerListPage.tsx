ad033c7c0db600293dba55ada1974bf60b810b81
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-02-01 22:42:15 +0545

    update list page

--- src/pages/banners/BannerListPage.tsx
+++ src/pages/banners/BannerListPage.tsx
@@ -1,12 +1,63 @@
-import { FaPen, FaTrash } from "react-icons/fa";
 import { TableHeader } from "../../components/ui/TableHeader";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { IBanner } from "./banner.contract";
 import { RowSkeleton } from "../../components/ui/table/TableSkeleton";
+import { toast } from "sonner";
+import axiosInstance from "../../config/axios.config";
+import type { IPagination, IResponse } from "../../common/GlobalType";
+import { RowAction } from "../../components/ui/table/RowAction";
+import { TablePagination } from "../../components/ui/table/TablePagination";
 
 export default function BannerListPage() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [banners, setBanners] = useState<Array<IBanner>>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [banners, setBanners] = useState<Array<IBanner>>([]);
+  const [pagination, setPagination] = useState<IPagination>({
+    limit: 20,
+    page: 1,
+    total: 0,
+    totalNoOfPages: 1
+  })
+
+  const getBannerList = async({page=1, limit=20, search=""}) => {
+    // data fetch from server
+    setLoading(true);
+    try {
+      const response: IResponse<IBanner> = await axiosInstance.get("/banner/", {
+        params: {
+          page: page,
+          limit: limit,
+          search: search
+        }
+      }) 
+      setBanners(response.data);
+      setPagination(response.meta.pagination)
+    } catch {
+      toast.error("Sorry! Could not fetch data!!!");
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      getBannerList({page:1, limit:20, search: ""});
+    }
+  }, [])
+
+  const onDeleteConfirm = async(id: string) => {
+    try {
+      setLoading(true);
+      await axiosInstance.delete("/banner/" + id);
+      toast.success("Banner deleted successfully");
+      await getBannerList({page:1, limit:20, search: ""});
+    } catch {
+      toast.error("Error deleting banner", {
+        description: "Something issue on deleting banner, please try again once... "
+      })
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <>
@@ -40,15 +91,15 @@ export default function BannerListPage() {
                     </>
                   : (
                     banners && banners.length ? (
-                      banners.map((banner) => (
-                        <tr key={banner.id} className="hover:bg-gray-50">
+                      banners.map((banner: IBanner, idx) => (
+                        <tr key={idx} className="hover:bg-gray-50">
                           <td className="p-3 font-medium text-gray-800">
                             {banner.title}
                           </td>
 
                           <td className="p-3">
                             <img
-                              src={banner.thumbnail}
+                              src={banner.image.url}
                               alt={banner.title}
                               className="w-25 rounded-md object-cover"
                             />
@@ -56,29 +107,24 @@ export default function BannerListPage() {
 
                           <td className="p-3">
                             <span
-                              className={`px-3 py-1 text-xs rounded-full ${banner.status === "Active"
-                                ? "bg-green-100 text-green-700"
-                                : "bg-red-100 text-red-700"
-                                }`}
+                              className={`px-3 py-1 text-xs rounded-full ${banner.status === "active" ? "bg-green-100 text-green-700" : "bg-red-100 text-red-700"}`}
                             >
-                              {banner.status}
+                              {banner.status === "active" ? "Published": "Un-Published"}
                             </span>
                           </td>
 
-                          <td className="p-3 text-teal-600 underline">
-                            {banner.url}
+                          <td className="p-3">
+                            <a href={banner.url} className=" text-blue-600 underline hover:text-blue-800" target="_banner" >
+                              {banner.url}
+                            </a>
                           </td>
 
                           <td className="p-3">
-                            <div className="flex justify-center gap-2">
-                              <button className="size-10 flex items-center justify-center rounded-full bg-teal-500 text-white hover:bg-teal-600 transition">
-                                <FaPen className="size-4" />
-                              </button>
-
-                              <button className="size-10 flex items-center justify-center rounded-full bg-red-500 text-white hover:bg-red-600 transition">
-                                <FaTrash className="size-4" />
-                              </button>
-                            </div>
+                            <RowAction 
+                              editUrl={"/admin/banner/" + banner._id}
+                              rowId={banner._id}
+                              onDeleteConfirm={onDeleteConfirm}
+                            />
                           </td>
                         </tr>
                       ))
@@ -98,33 +144,7 @@ export default function BannerListPage() {
           {/* Pagination */}
           {
             loading ? <></> : (
-              banners && <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-3 mt-6">
-            <p className="text-sm text-gray-600">
-              Showing 1 to 5 of 50 entries
-            </p>
-
-            <div className="flex gap-2">
-              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100 cursor-pointer">
-                Prev
-              </button>
-
-              <button className="px-3 py-1 rounded-lg bg-teal-500 text-white">
-                1
-              </button>
-
-              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100 cursor-pointer">
-                2
-              </button>
-
-              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100 cursor-pointer">
-                3
-              </button>
-
-              <button className="px-3 py-1 border rounded-lg text-gray-700 hover:bg-gray-100 cursor-pointer">
-                Next
-              </button>
-            </div>
-          </div>
+              banners && <TablePagination getDataAction={getBannerList} pagination={pagination} />
             )
           }
 
