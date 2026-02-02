b20b771da8b0525fb8c9fee14337aa17ba4c1b6c
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-02-02 15:18:02 +0545

    brand router creation

--- src/router/adminRouter.tsx
+++ src/router/adminRouter.tsx
@@ -1,6 +1,11 @@
 import BannerCreatePage from "../pages/banners/BannerCreatePage";
 import BannerEditPage from "../pages/banners/BannerEditPage";
 import BannerListPage from "../pages/banners/BannerListPage";
+
+import BrandCreatePage from "../pages/brands/BrandCreatePage";
+import BrandEditPage from "../pages/brands/BrandEditPage";
+import BrandListPage from "../pages/brands/BrandListPage";
+
 import UserDashboard from "../pages/dashboard/UserDashboard";
 import ErrorPage from "../pages/ErrorPage";
 import UserLayout from "../pages/layout/UserLayout";
@@ -13,6 +18,10 @@ export const adminRoutes = [{
     { path: "banners", element: <BannerListPage /> },
     { path: "banner/create", element: <BannerCreatePage /> },
     { path: "banner/:id", element: <BannerEditPage /> },
+
+    { path: "brands", element: <BrandListPage /> },
+    { path: "brand/create", element: <BrandCreatePage /> },
+    { path: "brand/:id", element: <BrandEditPage /> },
 
     { path: "user", element: <UserListPage /> },
     { path: "user/:userId", element: <UserListPage /> },
