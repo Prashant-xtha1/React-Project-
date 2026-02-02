61cb83a24a9159e7234833aa8ea5a48ec76f3a01
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-02-02 15:13:58 +0545

    User footer update

--- src/components/user-dashboard/UserFooter.tsx
+++ src/components/user-dashboard/UserFooter.tsx
@@ -1,5 +1,5 @@
 export const UserFooter = () => {
-  return(
+  return (
     <footer className="h-16 bg-gray-800 w-full text-white text-center flex items-center justify-center">
       <p className="text-sm">&copy; 2026 Prashant Kakshapati. All rights reserved.</p>
     </footer>
