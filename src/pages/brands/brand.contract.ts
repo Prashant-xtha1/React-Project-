e0604f1f1dd669da03e2911c8188570e5f84471a
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-02-02 15:11:50 +0545

    Brand contract created

---
+++ src/pages/brands/brand.contract.ts
@@ -0,0 +1,39 @@
+import z from "zod"
+import type { IImage } from "../../common/GlobalType"
+
+export interface IBrandCreateData {
+  name: string,
+  url: string,
+  status: string,
+  logo: File 
+}
+
+export interface IBrandEditData {
+  name: string,
+  url: string,
+  status: string,
+  logo?: File | null
+}
+
+export interface IBrand {
+  _id: string,
+  name: string,
+  url: string,
+  logo: IImage,
+  status: string,
+}
+
+export const BrandDTO = z.object({
+  name: z.string().min(3, "Atleast 3 character is required for name").nonempty("Title is required"),
+  url: z.url().nonempty("Url is required"),
+  status: z.string().regex(/^(active|inactive)$/, "Status can only be either Published or Un-Published").nonempty("Status is required"),
+  logo: z.file().nonoptional()
+})
+
+export const BrandEditDTO = z.object({
+  name: z.string().min(3, "Atleast 3 character is required for name").nonempty("Title is required"),
+  url: z.url().nonempty("Url is required"),
+  status: z.string().regex(/^(active|inactive)$/, "Status can only be either Published or Un-Published").nonempty("Status is required"),
+  logo: z.file().optional().nullable()
+})
+
