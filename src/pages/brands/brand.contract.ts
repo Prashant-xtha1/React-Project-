88da8afe119fd5999b7551588ac92711d639c184
Prashant-xtha1 <prince.kakshapati@gmail.com>
2026-02-03 13:44:49 +0545

    remove title and url

--- src/pages/brands/brand.contract.ts
+++ src/pages/brands/brand.contract.ts
@@ -3,14 +3,12 @@ import type { IImage } from "../../common/GlobalType"
 
 export interface IBrandCreateData {
   name: string,
-  url: string,
   status: string,
   logo: File 
 }
 
 export interface IBrandEditData {
   name: string,
-  url: string,
   status: string,
   logo?: File | null
 }
@@ -18,21 +16,18 @@ export interface IBrandEditData {
 export interface IBrand {
   _id: string,
   name: string,
-  url: string,
   logo: IImage,
   status: string,
 }
 
 export const BrandDTO = z.object({
   name: z.string().min(3, "Atleast 3 character is required for name").nonempty("Title is required"),
-  url: z.url().nonempty("Url is required"),
   status: z.string().regex(/^(active|inactive)$/, "Status can only be either Published or Un-Published").nonempty("Status is required"),
   logo: z.file().nonoptional()
 })
 
 export const BrandEditDTO = z.object({
   name: z.string().min(3, "Atleast 3 character is required for name").nonempty("Title is required"),
-  url: z.url().nonempty("Url is required"),
   status: z.string().regex(/^(active|inactive)$/, "Status can only be either Published or Un-Published").nonempty("Status is required"),
   logo: z.file().optional().nullable()
 })
